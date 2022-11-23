16ee290de673093824c8b8b7afea118139c5dc66
Austin C <acinindo@hushmail.com>
2022-11-23 11:57:43 -0800

    add php for contact form

---
+++ assets/contact form/mail.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "acinind0@hushmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
