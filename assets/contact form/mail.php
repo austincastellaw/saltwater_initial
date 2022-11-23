cfaa5aaf9103b54c4a7d09155dcbf45119513383
Austin C <acinindo@hushmail.com>
2022-11-23 12:33:51 -0800

    fixed email

--- assets/contact form/mail.php
+++ assets/contact form/mail.php
@@ -2,7 +2,7 @@
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "acinind0@hushmail.com";
+$recipient = "acinindo@hushmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
