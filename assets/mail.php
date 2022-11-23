23d6884f306152000d09f4d42ea3774605628dcb
Austin C <acinindo@hushmail.com>
2022-11-23 14:44:56 -0800

    debugging form

--- assets/mail.php
+++ assets/mail.php
@@ -1,16 +1,14 @@
 <?php 
-if (isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $message = $_POST['message'];
     $mailFrom = $_POST['email'];
 
     $mailTo = "acinindo@hushmail.com";
     $headers = "From: ".$mailFrom;
-    $txt = "you have recieved an email from" .$name.".\n\n .$message; 
+    $txt = "you have recieved an email from" .$name.".\n\n" .$message; 
 
     mail($mailTo, $txt, $headers);
     header("Location: index.html?mailsend")
-
-
 }
 ?>
